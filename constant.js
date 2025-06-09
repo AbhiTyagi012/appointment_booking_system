8a1fb56f4566931cc4ee2d317ced3879a94321b1
Abhi Tyagi <abhityagi@austere.co.in>
2025-06-10 00:05:04 +0530

    live backend link

--- constant.js
+++ constant.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-let domain = ["http://localhost:5050"];
+let domain = ["https://appointment-booking-system-ofsx.onrender.com"];
 
 let instance = axios.create({
   baseURL: domain[0] + "/api",
